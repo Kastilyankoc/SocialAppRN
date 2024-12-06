e4c11c2e057e0b07029a576541e1ac65c8f7f15d
Selim Koç <132671325+Kastilyankoc@users.noreply.github.com>
2024-12-07 01:53:11 +0300

    Update Location.js

--- util/Location.js
+++ util/Location.js
@@ -1,11 +1,1 @@
-const GOOGLE_API_KEY = 'Deneme';
 
-export default function getMapPreview(lat, lng, zoom = 14, size = "400x200", mapType = "roadmap") {
-  if (!lat || !lng) {
-    console.error("Invalid coordinates! Latitude and Longitude are required.");
-    return null;
-  }
-
-  const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=${size}&maptype=${mapType}&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
-  return imagePreviewUrl;
-}
